c11137f64718a80f4021ecd41c1394af055ea584
ilariamotta <ilaria.motta93@gmail.com>
2026-01-20 16:45:59 +0100

    feat: stampa lista film su pagina movies

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+
 export default function MoviesPage() {
+    const [movies, setMovies] = useState([]);
+    useEffect(()=> {
+        console.log("Chiamo Api");
+        axios.get("http://localhost:3000/api/movies").then((resp) => {console.log("Data:", resp.data); setMovies(resp.data.results)}).catch((err) =>{console.log(err)})
+    }, [])
     return (
+        <>
         <h1>Lista dei film</h1>
+        <ul>
+            {movies.map((movie)=>(
+                <li key={movie.id}>{movie.title}</li>
+            ))}        
+            </ul>
+            </>
     )
 }
