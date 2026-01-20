82700e13af9c921e2cc48009bbe867968cbf329b
ilariamotta <ilaria.motta93@gmail.com>
2026-01-20 12:54:16 +0100

    feat: componente header map link navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-
-
+const nomeApp = "Movies App";
   return (
     <>
+    <Header nomeApp={nomeApp} />
     <h1>Hello World</h1>
     </>
   )
